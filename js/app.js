4678559a25ab6b08d52ccbbfa30ea63e1bef6e33
chapmanCC <chapboy1989@gmail.com>
2019-03-31 07:53:24 +0800

    minor fix app.js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,40 @@
 window.addEventListener("DOMContentLoaded", () => {
 
     const directory = document.getElementById('directory');
+    const randomUserAPI = "https://randomuser.me/api/?results=12&nat=us,nz,au,ca";
 
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        const data = JSON.parse(xhr.responseText);
-        employeeDir(data.results);
-    }
-};
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            createEmployeeCard(data.results);
+        }
+    };
 
-xhr.open("GET", "https://randomuser.me/api/?results=12&nat=us,nz,au,ca", true);
-xhr.send();
-// const randomUserAPI = "https://randomuser.me/api/?results=12&nat=us,nz,au,ca";
+    xhr.open("GET", randomUserAPI, true);
+    xhr.send();
+})
 
-const employeeDir = employees => {
-    const directory = document.getElementById('directory');
+const createEmployeeCard = employees => {
     employees.forEach(employee => {
-                
+
         let employeeTel = createHTMLNode(employee.cell, "p", "card__employeeDetails", "card--hidden");
-        // console.log(employeeTel);
-        
+
         let location = employee.location.street + ", " + employee.location.city + ", " + employee.location.postcode;
         let employeeLocation = createHTMLNode(location, "p", "card__employeeDetails", "card--hidden");
         let dob = employee.dob.date.slice(0, 10);
         let employeeBday = createHTMLNode(dob, 'p', "card__employeeDetails", "card--hidden");
-        let cardSeperator = createHTMLNode(null , 'div', "card__seperator", "card--hidden");
+        let cardSeperator = createHTMLNode(null, 'div', "card__seperator", "card--hidden");
 
         let fullName = employee.name.first + " " + employee.name.last;
         let employeeCity = createHTMLNode(employee.email, "p", "card__employeeEmail");
         let employeeEmail = createHTMLNode(employee.location.city, 'p', "card__employeeDetails");
         let employeeName = createHTMLNode(fullName, 'h2', "card__employeeName");
-                    
+
         let cardProfile = createHTMLNode(null, "div", "card__profile");
 
         appendMultipleChild(cardProfile, employeeName, employeeEmail, employeeCity, cardSeperator, employeeTel, employeeLocation, employeeBday);
-        
+
         let img = createHTMLNode(null, 'img', "card__avatar");
         img.src = employee.picture.large;
         img.alt = fullName;
@@ -44,18 +43,16 @@ const employeeDir = employees => {
         appendMultipleChild(card, img, cardProfile);
 
         directory.appendChild(card);
-
     });
 };
 
-function createHTMLNode (innerText, element, ...classNames) { 
+function createHTMLNode(innerText, element, ...classNames) {
     let node = document.createElement(element);
     classNames.forEach(className => node.classList.add(className))
     node.innerText = innerText;
     return node;
 }
 
-function appendMultipleChild (parent, ...children) {
+function appendMultipleChild(parent, ...children) {
     children.forEach(child => parent.appendChild(child))
-}
-
+};
