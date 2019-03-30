0de2a0afea4f7f1ca6f203b2487203559bc4a437
chapmanCC <chapboy1989@gmail.com>
2019-03-31 07:35:49 +0800

    refactor app.js code

--- js/app.js
+++ js/app.js
@@ -1,61 +1,59 @@
-const directory = document.getElementById('directory');
+
 
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
         const data = JSON.parse(xhr.responseText);
-        const employees = data.results;
-        employees.forEach(employee => {
-            
-            let employeeTel = createHTMLNode("p", employee.cell, "card__employeeDetails card--hidden");
-            let location = employee.location.street + ", " + employee.location.city + ", " + employee.location.postcode;
-            let employeeLocation = createHTMLNode("p", location, "card__employeeDetails card--hidden");
-            let dob = employee.dob.date.slice(0, 10);
-            let employeeBday = createHTMLNode('p', dob, "card__employeeDetails card--hidden");
-            let cardSeperator = createHTMLNode('div', null , "card__seperator card--hidden");
-
-            let fullName = employee.name.first + " " + employee.name.last;
-            let employeeCity = createHTMLNode("p", employee.email, "card__employeeEmail");
-            let employeeEmail = createHTMLNode('p', employee.location.city, "card__employeeDetails");
-            let employeeName = createHTMLNode('h2', fullName, "card__employeeName");
-                        
-            let cardProfile = createHTMLNode("div", null, "card__profile");
-
-            cardProfile.appendChild(employeeName); // *TODO: try to create a function to append multiple children
-            cardProfile.appendChild(employeeEmail);
-            cardProfile.appendChild(employeeCity);
-            cardProfile.appendChild(cardSeperator);
-            cardProfile.appendChild(employeeTel);
-            cardProfile.appendChild(employeeLocation);
-            cardProfile.appendChild(employeeBday);
-            
-            let img = createHTMLNode('img', null, "card__avatar");
-            img.src = employee.picture.large;
-            img.alt = fullName;
-
-            let card = createHTMLNode('a', null, "card");
-            card.appendChild(img);
-            card.appendChild(cardProfile);
-
-            directory.appendChild(card);
-            
-
-        })
-        
-        
-        
-
+        employeeDir(data.results);
     }
-}
+};
+
 xhr.open("GET", "https://randomuser.me/api/?results=12&nat=us,nz,au,ca", true);
 xhr.send();
 // const randomUserAPI = "https://randomuser.me/api/?results=12&nat=us,nz,au,ca";
 
+const employeeDir = employees => {
+    const directory = document.getElementById('directory');
+    employees.forEach(employee => {
+                
+        let employeeTel = createHTMLNode(employee.cell, "p", "card__employeeDetails", "card--hidden");
+        // console.log(employeeTel);
+        
+        let location = employee.location.street + ", " + employee.location.city + ", " + employee.location.postcode;
+        let employeeLocation = createHTMLNode(location, "p", "card__employeeDetails", "card--hidden");
+        let dob = employee.dob.date.slice(0, 10);
+        let employeeBday = createHTMLNode(dob, 'p', "card__employeeDetails", "card--hidden");
+        let cardSeperator = createHTMLNode(null , 'div', "card__seperator", "card--hidden");
 
+        let fullName = employee.name.first + " " + employee.name.last;
+        let employeeCity = createHTMLNode(employee.email, "p", "card__employeeEmail");
+        let employeeEmail = createHTMLNode(employee.location.city, 'p', "card__employeeDetails");
+        let employeeName = createHTMLNode(fullName, 'h2', "card__employeeName");
+                    
+        let cardProfile = createHTMLNode(null, "div", "card__profile");
 
-function createHTMLNode (element, text, classNames) { //*TODO try use rest operator for classNames
+        appendMultipleChild(cardProfile, employeeName, employeeEmail, employeeCity, cardSeperator, employeeTel, employeeLocation, employeeBday);
+        
+        let img = createHTMLNode(null, 'img', "card__avatar");
+        img.src = employee.picture.large;
+        img.alt = fullName;
+
+        let card = createHTMLNode(null, 'a', "card");
+        appendMultipleChild(card, img, cardProfile);
+
+        directory.appendChild(card);
+
+    });
+};
+
+function createHTMLNode (innerText, element, ...classNames) { 
     let node = document.createElement(element);
-    node.className = classNames;
-    node.innerText = text;
+    classNames.forEach(className => node.classList.add(className))
+    node.innerText = innerText;
     return node;
 }
+
+function appendMultipleChild (parent, ...children) {
+    children.forEach(child => parent.appendChild(child))
+}
+
