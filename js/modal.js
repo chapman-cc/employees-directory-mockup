7048bccc8fea28c9d13a6f6f61851d1045061248
chapmanCC <chapboy1989@gmail.com>
2019-04-04 00:49:22 +0800

    final complete

--- js/modal.js
+++ js/modal.js
@@ -1,30 +1,29 @@
 directory.addEventListener('click', evt => {
     let card = evt.target.closest("div.card"); // select the nearest "div.card" element 
+    let modalRight = modal.querySelector("img.modal__rightArrow");
     let cardIndex = parseInt(card.dataset.in);
     let modalHTML = createCard(employeesDirectory[cardIndex], "large");
     modalHTML.dataset.in = cardIndex;
-    modal.appendChild(modalHTML);  
+    modal.insertBefore(modalHTML, modalRight)
     modal.classList.toggle('card--hidden')
     
 }, false)
 
 modal.addEventListener('click', evt => {
     if (evt.target === modal) {
-        modal.removeChild(modal.children[2]);
+        modal.removeChild(modal.children[1]);
         modal.classList.toggle("card--hidden")
     } else if (evt.target.tagName === "IMG") {
-        let bool = parseInt(evt.target.dataset.bool);
         
         let card = modal.querySelector("div.card");
         let cardIndex = parseInt(card.dataset.in);
-
+        
+        let bool = parseInt(evt.target.dataset.bool);
         let cardNewIndex = bool? cardIndex + 1 : cardIndex -1; 
         
         let modalHTML = createCard(employeesDirectory[cardNewIndex], "large");
         modalHTML.dataset.in = cardNewIndex;
-        modal.removeChild(modal.children[2]);
-        modal.appendChild(modalHTML);  
-        
+        modal.replaceChild(modalHTML, card)
     }
 })
 
